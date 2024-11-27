45c65272c484828c4bfae425e51bec3342611a57
Eliezer Reyes <Eliezer Reyes@DESKTOP-7918O69>
2024-11-26 18:48:33 -0600

    Agregar archivos de proyecto.

---
+++ Multiplicacion_De_Numeros/Controllers/CalculadoraController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiplicacionApp.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        // GET: Calculadora
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Resultado(int numero1, int numero2)
+        {
+            ViewBag.Resultado = numero1 * numero2;
+            return View("Index");
+        }
+    }
+}
---
+++ Multiplicacion_De_Numeros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Multiplicacion_De_Numeros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
